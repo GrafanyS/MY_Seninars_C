b39f7c698029971ea24a5af9354de7f2539b8ac4
GrafanyS <admin@valet.of.by>
2022-06-18 20:13:28 +0300

    Set added

---
+++ Seminar1/Set1/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Урок 1. Знакомство с языком программирования С#
+Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+
+Console.Clear();
+
+int a, b;
+
+Console.WriteLine("Введите два числа : ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (a > max) max = a;
+if (b > max) max = b;
+
+Console.Write("max = ");
+Console.WriteLine(max);
