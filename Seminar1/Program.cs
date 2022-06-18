85d3e21108d6670b717bf1fd35ec699258e4f395
GrafanyS <admin@valet.of.by>
2022-06-18 09:08:52 +0300

    Seminar1

---
+++ Seminar1/Program.cs
@@ -0,0 +1,28 @@
+﻿int n1, n2, quad;
+
+
+Console.Write("Input first namber: ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input second namber: ");
+n2 = Convert.ToInt32(Console.ReadLine());
+
+quad = n2 * n2;
+
+if (quad == n1)
+{
+    Console.Write("Yes");
+}
+else
+{
+    Console.Write("No");
+}
+
+
+
+
+
+
+
+
+
