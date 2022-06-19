f595c09ed4ef4a29bb3a9897b7029bf2b87f1a26
GrafanyS <admin@valet.of.by>
2022-06-19 22:50:36 +0300

    Семинар изменение добавлнно

--- Seminar1/Program.cs
+++ Seminar1/Program.cs
@@ -22,6 +22,59 @@ else
 
 
 
+class MyProgram
+{
+    static void Main()
+    {
+        int number;
+        string txt;
+        try
+        {
+            Console.WriteLine("Введите число от 1 до 7 включительно");
+
+            number = Convert.ToInt32(Console.ReadLine());
+            switch (number)
+            {
+                case 1:
+                    txt = "понедельник";
+                    break;
+                case 2:
+                    txt = "вторник";
+                    break;
+                case 3:
+                    txt = "среда";
+                    break;
+                case 4:
+                    txt = "четверг";
+                    break;
+                case 5:
+                    txt = "пятница";
+                    break;
+                case 6:
+                    txt = "суббота";
+                    break;
+                case 7:
+                    txt = "воскресение";
+                    break;
+                default:
+                    txt = "некорректное значение";
+                    break;
+            }
+            Console.WriteLine(txt, "День недели");
+        }
+        catch
+        {
+            Console.WriteLine("Вы не ввели число", "Ошибка");
+            Console.WriteLine("Завершение программы");
+        }
+    }
+}
+// дни недели.
+
+
+
+
+
 
 
 
