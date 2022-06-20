5ea036ae87e8fb6326a964268d83d968795c509e
GrafanyS <admin@valet.of.by>
2022-06-20 20:19:47 +0300

    Удалил папку Seminar1 содал новую папку Seminar изменил вложенные файлы

---
+++ Seminar/Seminar_lesson1/Task4/Program.cs
@@ -0,0 +1,33 @@
+﻿
+/*
+Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+
+using System;
+
+Console.Clear();
+
+int number;
+
+Console.WriteLine("Введите число");
+number = Convert.ToInt32(Console.ReadLine());
+
+int count = number * (-1);
+
+while (count <= number)
+{
+    if ((count > 0) && (count % 2 == 0))
+    {
+        Console.Write(count + "  ");
+        count++;
+    }
+    else
+    {
+        count++;
+    }
+}
+
