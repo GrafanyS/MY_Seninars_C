c1ebbf86d6725fcf6fc70636a412bbd6f0301950
GrafanyS <admin@valet.of.by>
2022-07-04 10:07:25 +0300

    Task29 + comments

--- Seminar/Seminar_lesson4/Task29/Program.cs
+++ Seminar/Seminar_lesson4/Task29/Program.cs
@@ -4,18 +4,20 @@
 
 
 
-void Random()
+void Random() // Random number generator
 {
     int number = new Random().Next(1, 8);  // random
     int[] numbers = new int[number];
     for (int i = 0; i < numbers.Length; i++)
     {
-        numbers[i] = new Random().Next(1, 100);
+        numbers[i] = new Random().Next(1, 100); // random number massive
     }
     // Console.WriteLine(numbers);
     // Console.Write($"{numbers[i]} ");
-    var str = string.Join(", ", numbers);
+    var str = string.Join(", ", numbers);// random numbers are assigned to the value of str  
     Console.WriteLine("[" + str + "]");
 
 }
-Random();
+Random(); //output of the Random method value
+
+// Создал коментарии с Яндекс переводчиком.
