700e1dfb68634bb34ad5134b4653e8a63140eb74
GrafanyS <admin@valet.of.by>
2022-07-06 16:00:40 +0300

    lesson4 commit подготовка к ДЗ

--- Seminar/Seminar_lesson5/lesson_seminar4/Program.cs
+++ Seminar/Seminar_lesson5/lesson_seminar4/Program.cs
@@ -2,7 +2,7 @@
 // числами из промежутка [-9, 9]. Найдите сумму отрицательных
 // и положительных элементов массива.
 
-
+/*
 
 int[] CreateRandomArray(int saize, int min, int max)
 {
@@ -23,7 +23,7 @@ void ShowArrays(int[] array)
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write(array[i] + " ");
-        
+
     }
     Console.WriteLine();
 
@@ -38,9 +38,9 @@ int FindPosiveSum(int[] array)
 }
 
 
-int[] myArray = CreateRandomArray(10, 1, 9);
-ShowArrays(myArray);
-Console.WriteLine("sum of proizve elements is" + FindPosiveSum(myArray));
+// int[] myArray = CreateRandomArray(10, 1, 9);
+// ShowArrays(myArray);
+// Console.WriteLine("sum of proizve elements is" + FindPosiveSum(myArray));
 
 
 
@@ -49,12 +49,14 @@ Console.WriteLine("sum of proizve elements is" + FindPosiveSum(myArray));
 // элементы замените на соответствующие отрицательные, и наоборот.
 
 // int[] array = CreateRandomArray(10, -10, 10);
+
+
 int[] m(int[] array)
 {
     
     for (int i = 1; i < array.Length; i++)
     {
-        array[i] = array[i * (-1)];
+        array[i] = array[i] * (-1);
     }
     return array;
 }
@@ -72,8 +74,53 @@ int max = Convert.ToInt32(Console.ReadLine());
 int[] mas = CreateRandomArray(size, min, max);
 
 ShowArrays(m(mas));
+*/
 
 
+/*
+
+// второй вариант: Напишите программу замена элементов массива: положительные
+// элементы замените на соответствующие отрицательные, и наоборот. С отображением генирируемых чисел.
+
+int[] NegativeMassive(int saize)
+{
+    int[] array = new int[saize];
+
+    for (int i = 0; i < saize; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    return array;
+
+}
+
+int[] ChangeArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+
+        array[i] = array[i] * (-1);
+
+    return array;
+}
+
+void ShowArrays(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int[] myArray =NegativeMassive(a);
+ChangeArray(myArray);
+ShowArrays(myArray);
+
+*/
 
 
 
@@ -87,6 +134,37 @@ ShowArrays(m(mas));
 // Задайте массив из 20 случайных чисел.
 //  Найдите количество элементов массива, значения которых лежат 
 // в отрезке [10,99].
+
+/*
+
+int[] Massive()
+{
+    int saize = 20;
+    int[] array = new int[saize];
+    for (int i = 0; i < saize; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+    return array;
+}
+
+int Elements(int[] array)
+{
+    int count = 0;//Присваем значение локальной переменной
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99) count++;
+    }
+    Console.WriteLine(count + " ");
+    return count;
+}
+
+int[] myArray = Massive();
+int result = Elements(myArray);
+
+*/
 
 
 
