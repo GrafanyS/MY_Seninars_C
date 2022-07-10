c6a17919153482136f8d415d7f33bf39d6896b1e
GrafanyS <admin@valet.of.by>
2022-07-10 17:00:43 +0300

    TASK34 испраил искал нечетные, ищет четные

--- Seminar/Seminar_lesson5/Task34/Program.cs
+++ Seminar/Seminar_lesson5/Task34/Program.cs
@@ -25,7 +25,7 @@ int QuantityPositive(int[] array)// метод определения четно
     int quantity = 0;
     for (int i = 0; i < array.Length; i++)
     {
-        if (array[i] % 2 == 1)
+        if (array[i] % 2 == 0)
         {
             quantity++;
         }
