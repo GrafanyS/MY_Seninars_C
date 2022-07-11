49a2146f4af2b3e0da66b533471bfe4143899c88
GrafanyS <admin@valet.of.by>
2022-07-11 17:16:15 +0300

    Task38 переделал в методы

--- Seminar/Seminar_lesson5/TAsk38/Program.cs
+++ Seminar/Seminar_lesson5/TAsk38/Program.cs
@@ -4,6 +4,8 @@
 
 // array of real numbers => массив вещественных чисел
 
+
+/*
 double[] arrayRealNumbers = new double[10];// генерируем 10 случайных чисел
 for (int i = 0; i < arrayRealNumbers.Length; i++)
 {
@@ -30,3 +32,67 @@ double decision = maxNumber - minNumber;//Находим разницу межд
 Console.WriteLine();
 
 Console.WriteLine($"разница между между максимальным ({maxNumber}) и минимальным({minNumber}) элементами: {decision}");//выводим в консоль значения
+*/
+
+
+Console.WriteLine("Введите длину масива: ");
+int count = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите min число: ");
+int min = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите max число: ");
+int max = Convert.ToInt32(Console.ReadLine());
+
+
+
+double[] arrayRealNumbers(int count, int min, int max)
+{
+    double[] array = new double[count];
+
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = (new Random().Next(min, max + 1)) + (new Random().NextDouble());        
+    }
+    return array;
+    
+}
+
+void ShowArrays(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");        
+    }
+    Console.WriteLine();
+}
+
+double SumOfNums(double[] array)
+{
+    double max = array[0];
+    double min = array[0];
+
+    for (int i = 1; i < array.Length; i++)//пробегаем циклом находя min и max
+    {
+        if (max < array[i])
+        {
+            max = array[i];
+        }
+        if (min > array[i])
+        {
+            min = array[i];
+        }
+    }
+    return max - min;
+    
+
+
+}
+
+double[] MyArray = arrayRealNumbers(count, min, max);
+ShowArrays(MyArray);
+Console.WriteLine();
+
+
+
+Console.WriteLine("разница между между максимальным и минимальным " + SumOfNums(MyArray));
