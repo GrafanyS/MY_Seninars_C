6a20b80ab63c6a43412da69f888804e310494e98
GrafanyS <admin@valet.of.by>
2022-07-18 22:14:03 +0300

    Task 52 решил

--- Seminar/Seminar_lesson7/Task52/Program.cs
+++ Seminar/Seminar_lesson7/Task52/Program.cs
@@ -9,14 +9,11 @@
 
 int[,] massive = { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };//заднный масив
 
-int m = 3;//строки
-int n = 4;//столбца
-
 void WriteArrayInt(int[,] massive)// метод
 {
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < massive.GetLength(0); i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < massive.GetLength(1); j++)
         {
             Console.Write(massive[i, j] + " ");
         }
@@ -24,20 +21,41 @@ void WriteArrayInt(int[,] massive)// метод
     }
 }
 
-Console.WriteLine();
 WriteArrayInt(massive);
-Console.WriteLine();
 
 Console.WriteLine($"Cреднее арифметическое: \n");
 
-for (int i = 0; i < n; i++)
+double[] Mean(int[,] value)
 {
-    double arithmetic = 0;
-    for (int j = 0; j < m; j++)
+    double[] mean = new double[value.GetLength(1)];
+    for (int i = 0; i < value.GetLength(1); i++)
     {
-        arithmetic += massive[j, i];
+        double sum = 0;
+        for (int j = 0; j < value.GetLength(0); j++)
+        {
+            sum += value[j, i];
+        }
+        mean[i] = Math.Round((double)sum / value.GetLength(0), 2);
+
     }
-    arithmetic = Math.Round(arithmetic / m, 2);
-    Console.WriteLine($"столбца № {i} {arithmetic}");
+    return mean;
 }
-Console.WriteLine();
+
+PrintMasively(Mean(massive));
+
+void Print(double[] values)
+{
+    for (int i = 0; i < values.Length; i++)
+        Console.Write(values[i] + " ");
+}
+void PrintMasively(double[] value)
+{
+    for(int i = 0; i < value.Length; i++)
+    {
+        if(value[i].ToString().Length>2)
+        Console.Write(value[i].ToString().Substring(0,3) + " ");
+        else
+        Console.Write(value[i].ToString() + " ");
+    }
+    
+}
