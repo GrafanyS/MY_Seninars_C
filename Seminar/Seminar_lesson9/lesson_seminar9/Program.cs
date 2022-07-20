080803bc6cd6a1842d990934c5d2f733f4f753a1
GrafanyS <admin@valet.of.by>
2022-07-20 17:02:29 +0300

    Lesson9 поправил добавил коментарии

--- Seminar/Seminar_lesson9/lesson_seminar9/Program.cs
+++ Seminar/Seminar_lesson9/lesson_seminar9/Program.cs
@@ -3,39 +3,42 @@
 
 // Задайте значение N. Напишите программу, которая выведет все 
 // натуральные числа в промежутке от 1 до N.
-/*
-void ShowNums(int n)
+
+Console.WriteLine();
+
+void ShowNums(int n)//метод который выдает от 1 до N.
 {
     if (n > 1) ShowNums(n - 1);
 
     Console.Write(n + " ");
 }
-*
-// (5)-> (4)->(3)->(2)->(1) wl(1)->(2) wl(2)->(3) wl(3)
+
+// (5) -> (4) -> (3) ->( 2) -> (1) wl(1) -> (2) wl(2) -> (3) wl(3) ->(4) wl(4) ->(5) wl(5)
 
 ShowNums(5);
 Console.WriteLine();
-*/
-/*
 
-void ShowNums1(int n)
+
+Console.WriteLine();
+void ShowNums1(int n) //метод который выдает от N до 1.
 {
-    if (n <= 1)
+    if (n >= 1)
     {
         Console.Write(n + " ");
-        ShowNums(n - 1);
-
+        ShowNums1(n - 1);
     }
-
 }
 
 ShowNums1(5);
-*/
+Console.WriteLine();
+
+
 
 // Напишите программу, которая будет принимать на вход число и возвращать 
 // сумму его цифр.
-/*
-int SumOfDigits(int n)
+
+
+int SumOfDigits(int n)// метод который суммирует чисо всех элементов
 {
     if (n >= 10)
     {
@@ -43,24 +46,27 @@ int SumOfDigits(int n)
     }
     else return n;
 }
-
+Console.WriteLine();
 Console.WriteLine(SumOfDigits(1234));
-*/
+
 // Задайте значения M и N. Напишите программу, которая выведет 
 // все натуральные числа в промежутке от M до N.
 
-// void ShowNums(int n, int m)
-// {
-//     if (n < m) ShowNums(n, m - 1);
+void ShowNums2(int n, int m)// метод упорядоченивания от меньшнго к большему
+{
+    if (n < m) ShowNums2(n, m - 1);
 
-//     Console.Write(m + " ");
-// }
-// ShowNums(2, 15);
+    Console.Write(m + " ");
+}
+Console.WriteLine();
+ShowNums2(2, 15);
+Console.WriteLine();
+
 
 // Напишите программу, которая на вход принимает два числа A и B, 
 // и возводит число А в целую степень B с помощью рекурсии.
 
-double Stepen(double a, double b)
+double Stepen(double a, double b)//метод возводит число А в целую степень B с помощью рекурсии
 {
     if (b > 0)
     {
@@ -73,4 +79,6 @@ double Stepen(double a, double b)
     else return 1;
 }
 
+Console.WriteLine();
 Console.WriteLine(Stepen(2, -3));
+Console.WriteLine();
