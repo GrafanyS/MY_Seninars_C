480a1b49fdd107367d559d22f679130873a7b62d
GrafanyS <admin@valet.of.by>
2022-07-22 10:42:24 +0300

    Task64 Напишите программу, которая найдет кол-во цифр в числе N рекурсивным методом

--- Seminar/Seminar_lesson9/Task64/Program.cs
+++ Seminar/Seminar_lesson9/Task64/Program.cs
@@ -18,23 +18,3 @@ void Print(string[] args)// метод вывода на печать
 
 Print(args);
 
-/*
-int n = InputNumbers("Введите n: ");
-int count = 2;
-PrintNumber(n, count);
-Console.Write(1);
-
-void PrintNumber(int n, int count)
-{
-    if (count > n) return;
-    PrintNumber(n, count + 1);
-    Console.Write(count + ", ");
-}
-
-int InputNumbers(string input)
-{
-    Console.Write(input);
-    int output = Convert.ToInt32(Console.ReadLine());
-    return output;
-}
-*/
