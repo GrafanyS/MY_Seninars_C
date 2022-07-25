f94a9b68c96f2fa1464c160ff1a108f1d4d33742
GrafanyS <admin@valet.of.by>
2022-07-25 23:13:40 +0300

    Task1

--- Seminar/Seminar_lesson10/Task1/Program.cs
+++ Seminar/Seminar_lesson10/Task1/Program.cs
@@ -3,3 +3,23 @@
 // Пример: { "qwe", "wer", "ert", "rty", "tyu"} -> 1
 
 
+string[] massive = { "qwe", "wer", "ert", "rty", "tyu" };
+
+int MassiveCount(string[] massive)// метод который считает кол-во слов в массиве, начинающихся на гласную букву.
+{
+    int count = 0;
+    for (int i = 0; i < massive.Length; i++)
+    {
+        if (massive[i][0] == 'a' ||
+            massive[i][0] == 'e' ||
+            massive[i][0] == 'i' ||
+            massive[i][0] == 'o' ||
+            massive[i][0] == 'u' ||
+            massive[i][0] == 'y')
+        {
+            count++;
+        }
+    }
+    return count;
+}
+Console.WriteLine(MassiveCount(massive));
